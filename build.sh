0445105456ca94f852c328e59dd3638e55595f8d
Milton Ngan <milton@valvesoftware.com>
2020-07-18 23:57:53 -0700

    Add tag to build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd $(dirname $(realpath $0))
 pm_dir="peering-manager"
 rm -rf ${pm_dir}
 git clone https://github.com/respawner/peering-manager.git ${pm_dir}
-docker build .
+docker build -t peering-manager .
