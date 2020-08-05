0e7c9d29b47dcb62ca03c40e6b012b1e75ef5536
Guillaume Mazoyer <guillaume@mazoyer.eu>
2020-08-05 22:37:19 +0200

    Some bling for `build.sh`

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
-#!/bin/sh 
-cd $(dirname $(realpath $0))
+#!/bin/bash
 
-pm_dir="peering-manager"
-rm -rf ${pm_dir}
-git clone https://github.com/respawner/peering-manager.git ${pm_dir}
-docker build -t peering-manager .
+URL="https://github.com/respawner/peering-manager.git"
+PEERING_MANAGER_PATH="peering-manager"
+
+echo "▶️ $0 $*"
+
+set -e
+
+echo "👓 Going in the right location"
+cd $(dirname $(realpath $0))
+echo "✅ We are there"
+
+echo "🌀 Cleaning up remains of last build"
+rm -rf ${PEERING_MANAGER_PATH}
+echo "✅ All cleaned"
+
+echo "🌐 Checking out 'master' of Peering Manager from '${URL}' into '${PEERING_MANAGER_PATH}'"
+git clone ${URL} ${peering_manager_directory}
+echo "✅ Checked out Peering Manager"
+
+echo "🐳 Building Docker image"
+docker build -t peering-manager --build-arg PEERING_MANAGER_PATH=./${PEERING_MANAGER_PATH} .
+echo "✅ Docker image ready"
