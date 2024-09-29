5a7b4182d4b5978b9b1109461d912919889f8961
Guillaume Mazoyer <guillaume@mazoyer.eu>
2024-09-29 15:18:26 +0200

    Refactor testing for more complete test suite

--- test.sh
+++ test.sh
@@ -2,13 +2,17 @@
 # Runs the Peering Manager unit tests.
 # Usage:
 #   ./test.sh latest
-#   ./test.sh v1.5.2
+#   ./test.sh v1.8.3
 #   IMAGE='peeringmanager/peering-manager:latest' ./test.sh
-#   IMAGE='peeringmanager/peering-manager:v1.5.2' ./test.sh
+#   IMAGE='peeringmanager/peering-manager:v1.8.3' ./test.sh
 
 # exit when a command exits with an exit code != 0
 set -e
 
+source ./build-functions/gh-functions.sh
+
+# IMAGE is used by `docker-compose.yml` do determine the tag
+# of the Docker Image that is to be used
 if [ "${1}x" != "x" ]; then
   # Use the command line argument
   export IMAGE="peeringmanager/peering-manager:${1}"
@@ -28,11 +32,13 @@ if [ -z "${IMAGE}" ]; then
 fi
 
 # The docker compose command to use
-doco="docker compose --file docker-compose.test.yml --project-name peeringmanager_docker_test_${1}"
+doco="docker compose --file docker-compose.test.yml --file docker-compose.test.override.yml --project-name peeringmanager_docker_test"
 INITIALIZERS_DIR=".initializers"
 
 test_setup() {
+  gh_echo "::group:: Test setup"
   echo "🏗  Setup up test environment"
+
   if [ -d "${INITIALIZERS_DIR}" ]; then
     rm -rf "${INITIALIZERS_DIR}"
   fi
@@ -44,22 +50,68 @@ test_setup() {
       sed -E 's/^# //' "${script}" > "../${INITIALIZERS_DIR}/${script}"
     done
   )
+
+  $doco up --detach --quiet-pull --wait --force-recreate --renew-anon-volumes --no-start
+  $doco start postgres
+  $doco start redis
+  $doco start redis-cache
+  gh_echo "::endgroup::"
 }
 
 test_peeringmanager_unit_tests() {
+  gh_echo "::group:: Peering Manager unit tests"
   echo "⏱  Running Peering Manager Unit Tests"
   $doco run --rm peering-manager /opt/peering-manager/venv/bin/python /opt/peering-manager/manage.py test
+  gh_echo "::endgroup::"
 }
 
-test_initializers() {
-  echo "🏭 Testing Initializers"
-  export INITIALIZERS_DIR
-  $doco run --rm peering-manager /opt/peering-manager/docker-entrypoint.sh ./manage.py check
+test_compose_db_setup() {
+  gh_echo "::group:: Peering Manager database migrations"
+  echo "⏱ Running Peering Manager database migrations"
+  $doco run --rm peering-manager /opt/peering-manager/venv/bin/python /opt/peering-manager/manage.py migrate
+  gh_echo "::endgroup::"
+}
+
+test_peeringmanager_start() {
+  gh_echo "::group:: Start Peering Manager service"
+  echo "⏱ Starting Peering Manager services"
+  $doco up --detach --wait
+  gh_echo "::endgroup::"
+}
+
+test_peeringmanager_web() {
+  gh_echo "::group:: Web service test"
+  echo "⏱ Starting web service test"
+  RESP_CODE=$(
+    curl \
+      --silent \
+      --output /dev/null \
+      --write-out '%{http_code}' \
+      --request GET \
+      --connect-timeout 5 \
+      --max-time 10 \
+      --retry 5 \
+      --retry-delay 0 \
+      --retry-max-time 40 \
+      http://localhost:8000/login/
+  )
+  if [ "${RESP_CODE}" == "200" ]; then
+    echo "✅ Web service running"
+  else
+    echo "⚠️ Got response code '${RESP_CODE}' but expected '200'"
+    exit 1
+  fi
+  gh_echo "::endgroup::"
 }
 
 test_cleanup() {
   echo "💣 Cleaning Up"
-  $doco down -v
+  gh_echo "::group:: Docker compose logs"
+  $doco logs --no-color
+  gh_echo "::endgroup::"
+  gh_echo "::group:: Docker compose down"
+  $doco down --volumes
+  gh_echo "::endgroup::"
 
   if [ -d "${INITIALIZERS_DIR}" ]; then
     rm -rf "${INITIALIZERS_DIR}"
@@ -73,7 +125,8 @@ trap test_cleanup EXIT ERR
 test_setup
 
 test_peeringmanager_unit_tests
-# FIXME: initializers disabled until new release
-# test_initializers
+test_compose_db_setup
+test_peeringmanager_start
+test_peeringmanager_web
 
 echo "🐳 Done testing '${IMAGE}'"
